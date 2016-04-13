c0736767c61b4cebff1e1475e620257d4b7cdfe0
Zachary Olsen <zolsen1@x9da10.harford.edu>
2016-04-13 17:38:31 -0400

    js added for states and pr

--- js/main.js
+++ js/main.js
@@ -6,4 +6,312 @@ $(document).ready( function() {
     });
   });
 
+  $("#maryland").click( function() {
+    $("body").css({
+      "backgroundImage": "url('Images/mdcap.jpg')"
+    });
+  });
+
+  $("#california").click( function() {
+    $("body").css({
+      "backgroundImage": "url('Images/calcap.jpg')"
+    });
+  });
+
+  $("#alabama").click( function() {
+    $("body").css({
+      "backgroundImage": "url('Images/alcap.jpg')"
+    });
+  });
+
+  $("#alaska").click( function() {
+    $("body").css({
+      "backgroundImage": "url('Images/alaskcap.jpg')"
+    });
+  });
+
+  $("#arizona").click( function() {
+    $("body").css({
+      "backgroundImage": "url('Images/azcap.jpg')"
+    });
+  });
+
+  $("#arkansas").click( function() {
+    $("body").css({
+      "backgroundImage": "url('Images/arkcap.jpg')"
+    });
+  });
+
+  $("#colorado").click( function() {
+    $("body").css({
+      "backgroundImage": "url('Images/colcap.jpg')"
+    });
+  });
+
+  $("#connecticut").click( function() {
+    $("body").css({
+      "backgroundImage": "url('Images/ctcap.jpg')"
+    });
+  });
+
+  $("#delaware").click( function() {
+    $("body").css({
+      "backgroundImage": "url('Images/decap.jpg')"
+    });
+  });
+
+  $("#georgia").click( function() {
+    $("body").css({
+      "backgroundImage": "url('Images/gecap.jpg')"
+    });
+  });
+
+  $("#hawaii").click( function() {
+    $("body").css({
+      "backgroundImage": "url('Images/hwcap.jpg')"
+    });
+  });
+
+  $("#idaho").click( function() {
+    $("body").css({
+      "backgroundImage": "url('Images/idcap.jpg')"
+    });
+  });
+
+  $("#illinois").click( function() {
+    $("body").css({
+      "backgroundImage": "url('Images/illcap.jpg')"
+    });
+  });
+
+  $("#indiana").click( function() {
+    $("body").css({
+      "backgroundImage": "url('Images/Indianapolis.jpg')"
+    });
+  });
+
+  $("#iowa").click( function() {
+    $("body").css({
+      "backgroundImage": "url('Images/iowacap.jpg')"
+    });
+  });
+
+  $("#kansas").click( function() {
+    $("body").css({
+      "backgroundImage": "url('Images/kacap.jpg')"
+    });
+  });
+
+  $("#kentucky").click( function() {
+    $("body").css({
+      "backgroundImage": "url('Images/kycap.jpg')"
+    });
+  });
+
+  $("#louisiana").click( function() {
+    $("body").css({
+      "backgroundImage": "url('Images/louscap.jpg')"
+    });
+  });
+
+  $("#maine").click( function() {
+    $("body").css({
+      "backgroundImage": "url('Images/maine.jpg')"
+    });
+  });
+
+  $("#mass").click( function() {
+    $("body").css({
+      "backgroundImage": "url('Images/masscap.jpg')"
+    });
+  });
+
+  $("#michigan").click( function() {
+    $("body").css({
+      "backgroundImage": "url('Images/michcap.jpg')"
+    });
+  });
+
+  $("#minnesota").click( function() {
+    $("body").css({
+      "backgroundImage": "url('Images/minncap.jpg')"
+    });
+  });
+
+  $("#mississ").click( function() {
+    $("body").css({
+      "backgroundImage": "url('Images/misscap.jpg')"
+    });
+  });
+
+  $("#missouri").click( function() {
+    $("body").css({
+      "backgroundImage": "url('Images/missouricap.jpg')"
+    });
+  });
+
+  $("#montana").click( function() {
+    $("body").css({
+      "backgroundImage": "url('Images/montanacap.jpg')"
+    });
+  });
+
+  $("#nebraska").click( function() {
+    $("body").css({
+      "backgroundImage": "url('Images/nebracap.jpg')"
+    });
+  });
+
+  $("#nevada").click( function() {
+    $("body").css({
+      "backgroundImage": "url('Images/nevadacap.JPG')"
+    });
+  });
+
+  $("#newhamp").click( function() {
+    $("body").css({
+      "backgroundImage": "url('Images/nhcap.JPG')"
+    });
+  });
+
+  $("#newjer").click( function() {
+    $("body").css({
+      "backgroundImage": "url('Images/njcap.jpg')"
+    });
+  });
+
+  $("#newmex").click( function() {
+    $("body").css({
+      "backgroundImage": "url('Images/nmcap.jpeg')"
+    });
+  });
+
+  $("#newyork").click( function() {
+    $("body").css({
+      "backgroundImage": "url('Images/nycap.jpg')"
+    });
+  });
+
+  $("#northcaro").click( function() {
+    $("body").css({
+      "backgroundImage": "url('Images/nccap.jpeg')"
+    });
+  });
+
+  $("#northdak").click( function() {
+    $("body").css({
+      "backgroundImage": "url('Images/nordakcap.jpg')"
+    });
+  });
+
+  $("#ohio").click( function() {
+    $("body").css({
+      "backgroundImage": "url('Images/ohiocap.jpg')"
+    });
+  });
+
+  $("#oklahoma").click( function() {
+    $("body").css({
+      "backgroundImage": "url('Images/okcap.jpg')"
+    });
+  });
+
+  $("#oregon").click( function() {
+    $("body").css({
+      "backgroundImage": "url('Images/orgcap.jpg')"
+    });
+  });
+
+  $("#penn").click( function() {
+    $("body").css({
+      "backgroundImage": "url('Images/pacap.jpg')"
+    });
+  });
+
+  $("#rhode").click( function() {
+    $("body").css({
+      "backgroundImage": "url('Images/ricap.jpg')"
+    });
+  });
+
+  $("#southcaro").click( function() {
+    $("body").css({
+      "backgroundImage": "url('Images/sccap.jpg')"
+    });
+  });
+
+  $("#southdak").click( function() {
+    $("body").css({
+      "backgroundImage": "url('Images/soudakcap.jpg')"
+    });
+  });
+
+  $("#tennessee").click( function() {
+    $("body").css({
+      "backgroundImage": "url('Images/tenncap.jpg')"
+    });
+  });
+
+  $("#texas").click( function() {
+    $("body").css({
+      "backgroundImage": "url('Images/texcap.jpg')"
+    });
+  });
+
+  $("#utah").click( function() {
+    $("body").css({
+      "backgroundImage": "url('Images/utahcap.jpg')"
+    });
+  });
+
+  $("#vermont").click( function() {
+    $("body").css({
+      "backgroundImage": "url('Images/vtcap.jpg')"
+    });
+  });
+
+  $("#virginia").click( function() {
+    $("body").css({
+      "backgroundImage": "url('Images/vacap.jpg')"
+    });
+  });
+
+
+  $("#washington").click( function() {
+    $("body").css({
+      "backgroundImage": "url('Images/washcap.jpg')"
+    });
+  });
+
+  $("#westvirginia").click( function() {
+    $("body").css({
+      "backgroundImage": "url('Images/wvcap.jpg')"
+    });
+  });
+
+  $("#wyoming").click( function() {
+    $("body").css({
+      "backgroundImage": "url('Images/wyomcap.jpg')"
+    });
+  });
+
+  $("#puerto").click( function() {
+    $("body").css({
+      "backgroundImage": "url('Images/prcap.jpg')"
+    });
+  });
+
+
+  $("#dc").click( function() {
+    $("body").css({
+      "backgroundImage": "url('Images/dccap.jpg')"
+    });
+  });
+
+
+
+
+
+
+
 });
